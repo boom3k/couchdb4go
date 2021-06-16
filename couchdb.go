be56221072664d9fe554f541c56d4a6299423c06
Ramel Henderson <boomboom3k@gmail.com>
2021-06-16 17:42:14 -0400

    Cleaned up most objects

--- couchdb.go
+++ couchdb.go
@@ -1,7 +1,9 @@
-package couchdb4go
+package main
 
 import (
 	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -9,81 +11,11 @@ import (
 	"strings"
 )
 
-type CouchDB struct {
-	ServerAddress    string
-	Username         string
-	Password         string
-	IsSecureServer   bool
-	ActiveConnection bool
-	Request          *http.Request
+func main() {
+	couch := NewSession("root", "boomer", "10.0.0.51", false)
+	couch.CreateDatabase("dev", true)
 }
 
-// Initialize Creates a new running couchdb object
-func Initialize(username, password, serverAddress string, secureSever bool) *CouchDB {
-	database := &CouchDB{}
-	port := 5984
-	httpType := "http://"
-	if secureSever {
-		httpType = "https://"
-	}
-	database.ServerAddress = httpType + serverAddress + ":" + fmt.Sprint(port) + "/"
-	database.Username = username
-	database.Password = password
-	database.IsSecureServer = secureSever
-	database.ActiveConnection = database.VerifyConnection()
-	return database
-}
-
-func (c *CouchDB) SetRequest(method, ask string, body []byte) *CouchDB {
-	request, err := http.NewRequest(strings.ToUpper(method), c.ServerAddress+ask, bytes.NewBuffer(body))
-	if err != nil {
-		log.Println(err.Error())
-		panic(err)
-	}
-	c.Request = request
-	return c
-}
-
-func (c *CouchDB) VerifyConnection() bool {
-	c.SetRequest("GET", "", nil)
-	response, err := c.Do()
-	if err != nil {
-		log.Println(err.Error())
-		panic(err)
-	}
-	log.Printf("Connection to <%s> [%s]\n", c.ServerAddress, response.Status)
-	if response.StatusCode == 200 {
-		return true
-	}
-	return false
-}
-
-// Do Request Handler
-func (c *CouchDB) Do() (*http.Response, error) {
-	c.Request.SetBasicAuth(c.Username, c.Password)
-	c.Request.Header.Set("Content-Type", "application/json; charset=UTF-8")
-
-	reader := new(bytes.Buffer)
-	reader.ReadFrom(c.Request.Body)
-	log.Println(c.Request.Method + ": " + reader.String() + " -> " + fmt.Sprint(c.Request.URL))
-	httpClient := &http.Client{}
-	response, err := httpClient.Do(c.Request)
-	c.Request = nil
-	if err != nil {
-		log.Println(err.Error())
-		panic(err)
-	}
-	responseBody, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Println(err.Error())
-		return response, err
-	}
-
-	response.Proto = string(responseBody)
-	return response, nil
-}
-
-// ExecuteURL For testing db calls
 func ExecuteURL(method, username, password, url string, body []byte) (*http.Response, error) {
 	var httpClient http.Client
 	request, err := http.NewRequest(method, url, bytes.NewBuffer(body))
@@ -108,14 +40,153 @@ func ExecuteURL(method, username, password, url string, body []byte) (*http.Resp
 	return response, nil
 }
 
-/*Insert Methods*/
-func (c *CouchDB) CreateDatabase(databaseName string, isPartitioned bool) (*http.Response, error) {
-	c.SetRequest("PUT", databaseName+"?partitioned="+fmt.Sprint(isPartitioned), nil)
-	return c.Do()
+type Session struct {
+	ServerAddress    string
+	Username         string
+	Password         string
+	IsSecureServer   bool
+	ActiveConnection bool
+	RequestHandler   *http.Request
 }
 
-func (c *CouchDB) CreateDocument(database string, jsonData []byte) (*http.Response, error) {
+func NewSession(username, password, serverAddress string, secureSever bool) *Session {
+	session := &Session{}
+	port := 5984
+	httpType := "http://"
+	if secureSever {
+		httpType = "https://"
+	}
+	session.ServerAddress = httpType + serverAddress + ":" + fmt.Sprint(port) + "/"
+	session.Username = username
+	session.Password = password
+	session.IsSecureServer = secureSever
+	session.ActiveConnection = session.EstablishConnection()
+	return session
+}
 
-	c.SetRequest("POST", database, jsonData)
-	return c.Do()
+type Database struct {
+	Name    string
+	Session *Session
+}
+
+func (database *Database) GetDatabase(databaseName string, session *Session) *Database {
+	database.Name = databaseName
+	database.Session = session
+	return database
+}
+
+type Res struct {
+	Status  int
+	JsonMap map[string]interface{}
+	Body    string
+}
+
+func GetResponse(response *http.Response) *Res {
+	var cr = &Res{}
+	cr.Status = response.StatusCode
+	buffer := new(bytes.Buffer)
+	buffer.ReadFrom(response.Body)
+	cr.Body = buffer.String()
+	err := json.Unmarshal([]byte(cr.Body), &cr.JsonMap)
+	if err != nil {
+		log.Println(err.Error())
+		panic(err)
+	}
+	if cr.JsonMap["error"] != nil {
+		err = errors.New(cr.JsonMap["reason"].(string))
+	}
+	return cr
+}
+
+func (session *Session) Do() (*Res, error) {
+	session.RequestHandler.SetBasicAuth(session.Username, session.Password)
+	session.RequestHandler.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	buffer := new(bytes.Buffer)
+	buffer.ReadFrom(session.RequestHandler.Body)
+	httpClient := http.Client{}
+	httpResponse, err := httpClient.Do(session.RequestHandler)
+	if err != nil {
+		log.Println(err.Error())
+		return nil, err
+	}
+	resultMsg := session.RequestHandler.Method + ": " + buffer.String() + " {" + fmt.Sprint(session.RequestHandler.URL) + "} | Status: " + fmt.Sprint(httpResponse.StatusCode)
+	response := GetResponse(httpResponse)
+	if response.JsonMap["error"] != nil {
+		resultMsg += " -- " + (response.JsonMap["reason"]).(string)
+	}
+	log.Println(resultMsg)
+	session.RequestHandler = nil
+	return response, nil
+}
+
+func (session *Session) EstablishConnection() bool {
+	session.SetRequest("GET", "", nil)
+	response, err := ExecuteURL("GET", session.Username, session.Password, session.ServerAddress, nil)
+	if err != nil {
+		log.Println(err.Error())
+		panic(err)
+	}
+	log.Printf("Connection to <%s> [%s]\n", session.ServerAddress, response.Status)
+	if response.StatusCode == 200 {
+		return true
+	}
+	return false
+}
+
+func (session *Session) SetRequest(method, ask string, body []byte) *Session {
+	request, err := http.NewRequest(strings.ToUpper(method), session.ServerAddress+ask, bytes.NewBuffer(body))
+	if err != nil {
+		log.Println(err.Error())
+		panic(err)
+	}
+	session.RequestHandler = request
+	return session
+}
+
+func (session *Session) CreateDatabase(databaseName string, isPartitioned bool) (*Database, error) {
+	_, err := session.SetRequest("PUT", databaseName+"?partitioned="+fmt.Sprint(isPartitioned), nil).Do()
+	if err != nil {
+		log.Println(err.Error())
+		return nil, err
+	}
+	db := &Database{}
+	db.GetDatabase(databaseName, session)
+	return db, nil
+}
+
+func (database *Database) CreateDocument(jsonData []byte) (*Res, error) {
+	res, err := database.Session.SetRequest("POST", database.Name, jsonData).Do()
+	if err != nil {
+		log.Println(err.Error())
+		return nil, err
+	}
+
+	return res, err
+}
+
+func (database *Database) ReadDocument(_id string) (map[string]interface{}, error) {
+	response, err := database.Session.SetRequest("GET", database.Name+"/"+_id, nil).Do()
+	if err != nil {
+		log.Println(err.Error())
+		return nil, err
+	}
+	return response.JsonMap, nil
+}
+
+func (database *Database) UpdateDocument(_id string, jsonData []byte) (*Res, error) {
+	_rev, err := database.ReadDocument(_id)
+	if err != nil {
+		log.Println(err.Error())
+		panic(err)
+	}
+	return database.Session.SetRequest("PUT", database.Name+"/"+_id+"?rev="+_rev["_rev"].(string), jsonData).Do()
+}
+
+func (database *Database) DeleteDocument(_id string) (*Res, error) {
+	_rev, err := database.ReadDocument(_id)
+	if err != nil {
+		log.Println(err.Error())
+		panic(err)
+	}
+	return database.Session.SetRequest("DELETE", database.Name+"/"+_id+"?rev="+_rev["_rev"].(string), nil).Do()
 }
